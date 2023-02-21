99106aa3f2f1fd5587ba2cb56650b5cb582cec4f
Kle012 <kimhle.0102@gmail.com>
2023-02-20 18:51:49 -0800

    RegisterForm and LoginForm

---
+++ forms.py
@@ -0,0 +1,16 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField
+from wtforms.validators import InputRequired, Email
+
+class RegisterForm (FlaskForm):
+    """Form for adding user."""
+    first_name = StringField('First Name', validators=[InputRequired()])
+    last_name = StringField('Last Name', validators=[InputRequired()])
+    username = StringField('Username', validators=[InputRequired()])
+    password = PasswordField('Password', validators=[InputRequired()])
+    email = StringField('E-mail', validators=[InputRequired(), Email()])
+
+class LoginForm (FlaskForm):
+    """Form for user to log in."""
+    username = StringField('Username', validators = [InputRequired()])
+    password = PasswordField('Password', validators = [InputRequired()])
