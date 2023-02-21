696dfe3cc91d8fb7a7e20527c34e9cbde69cd2f4
Kle012 <kimhle.0102@gmail.com>
2023-02-20 19:14:32 -0800

    Post form

--- forms.py
+++ forms.py
@@ -1,5 +1,5 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField
+from wtforms import StringField, PasswordField, TextAreaField
 from wtforms.validators import InputRequired, Email
 
 class RegisterForm (FlaskForm):
@@ -14,3 +14,7 @@ class LoginForm (FlaskForm):
     """Form for user to log in."""
     username = StringField('Username', validators = [InputRequired()])
     password = PasswordField('Password', validators = [InputRequired()])
+
+class PostForm (FlaskForm):
+    """Form to add a post."""
+    text = TextAreaField('Post', validators=[InputRequired()])
